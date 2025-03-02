4713caf4f11f8ef6621a97abcd9893ae622d8074
Peeranat Chunhok <Peeranat280648@gmail.com>
2025-03-02 11:11:09 +0700

    Added funnyString function

---
+++ funnyString.py
@@ -0,0 +1,12 @@
+def funnyString(s):
+    # Write your code here
+    r = s[::-1]
+
+    diff_s = [abs(ord(s[i]) - ord(s[i + 1])) for i in range(len(s) - 1)]
+
+    diff_r = [abs(ord(r[i]) - ord(r[i + 1])) for i in range(len(r) - 1)]
+
+    if diff_s == diff_r:
+        return "Funny"
+    else:
+        return "Not Funny"
